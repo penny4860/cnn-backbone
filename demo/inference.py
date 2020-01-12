4d2ef664c63a0a81d04a5f3eae1deb8a7ad1514f
jeongjoonsup <penny4860@gmail.com>
2020-01-12 14:08:40 +0900

    demo script 파일 renamed

--- demo/inference.py
+++ demo/inference.py
@@ -9,21 +9,21 @@ from keras_applications import PROJECT_ROOT
 from keras.preprocessing import image
 
 DATASET_ROOT = os.path.join(os.path.dirname(PROJECT_ROOT), "dataset", "cifar100", "train")
-# img_path = glob.glob(DATASET_ROOT + "/*/*.png")[0]
+IMAGE_PATH = glob.glob(DATASET_ROOT + "/apple/*.png")[0]
 
 if __name__ == '__main__':
 
     model = EfficientNetB1(weights='imagenet')
     _, height, width, _ = model.input_shape
 
-    img_path = os.path.join(PROJECT_ROOT, "imgs", "img2.jpg")
-    img = image.load_img(img_path, target_size=(height, width))
+    img = image.load_img(IMAGE_PATH, target_size=(height, width))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
+    print(x.shape)
 
     preds = model.predict(x)
     results = decode_predictions(preds, top=3)[0]
 
     print('Predicted:', results)
-    print(img_path)
+    print(IMAGE_PATH)
